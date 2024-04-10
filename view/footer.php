79662eb87723ed9defddbf6b76299ab3902fb45e
khaitvph33695 <150763010+khaitvph33695@users.noreply.github.com>
2024-04-10 21:41:00 +0700

    init

---
+++ view/footer.php
@@ -0,0 +1,43 @@
+<!-- begin footer -->
+<footer>
+  <!-- Copyright © Classic Store
+  GPĐK HKD: 41O8047539 do UBND Quận Bình Thạnh cấp ngày 27 tháng 04 năm 2023
+   -->
+  <div class="kele"></div>
+  <div class="site-footers">
+    <div class="site-footer">
+      <h5>Shoe Store</h5>
+      <p>Được thành lập từ năm 2023. Shoe Store đã trở thành địa chỉ mua sắm những sản phẩm giày bóng đá, giày chạy bộ, giày leo núi uy tín dành riêng cho mọi giới.</p>
+    </div>
+    <div class="site-footer">
+      <h5>ĐIỀU KHOẢN DỊCH VỤ</h5>
+      <li><a href="index.php?act=chinhsachquydinh">Chính sách & quy định</a></li>
+      <li><a href="">Chính sách thanh toán</a></li>
+      <li><a href="">Chính sách đổi trả</a></li>
+      <li><a href="">Chính sách bảo mật thông tin</a></li>
+
+    </div>
+    <div class="site-footer">
+      <h5>THEO DÕI CHÚNG TÔI</h5>
+      <li><a href=""><img src="./img/logofb.png" alt="">Facebook</a></li>
+      <li> <a href=""><img src="./img/logo-instagram.png" alt="">Instagram</a></li>
+    </div>
+  </div>
+  <div class="footer-content">
+    <p>Shoe_Store.vn</p>
+    <p>Địa chỉ: Tầng 1, Tòa nhà Capital Place, số 29 đường Liễu Giai, Phường Ngọc Khánh, Quận Ba Đình, Thành phố Hà Nội, Việt Nam. Tổng đài hỗ trợ: 19001221 - Email: cskh@hotro.shoestore.vn
+      Chịu Trách Nhiệm Quản Lý Nội Dung: Điện thoại liên hệ: 0732344254
+      Mã số doanh nghiệp: 0106734793 do Sở Kế hoạch & Đầu tư TP Hà Nội cấp lần đầu ngày 10/11/2023</p>
+  </div>
+</footer>
+
+</body>
+<script>
+    document.getElementById('backButton').addEventListener('click', function() {
+        // Quay lại trang trước
+        window.history.back();
+    });
+</script>
+
+</html>
+<!-- end footer -->
